bad1a584a6dbd95c9a2018376170393d168af3d7
Bridget <bridgetoconnor808@gmail.com>
2019-01-18 20:42:32 -0800

    this is a file for u ameya

---
+++ HelloAmeya.java
@@ -0,0 +1,11 @@
+public class HelloAmeya{
+  public static void main(String[] args){
+    String result = "Hi Ameya you're so cool \n";
+    System.out.println(result);
+    for (int i = 0; i < 100; i++){
+      result += " really\n";
+    }
+    result += "cool";
+    System.out.println(result);
+  }
+}
