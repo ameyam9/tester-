e2fd798b8bd632a7c43a0acf6a6fa00a2806fda1
Bridget <bridgetoconnor808@gmail.com>
2019-01-18 21:23:48 -0800

    More Vegetables

--- vegetables.java
+++ vegetables.java
@@ -1,22 +1,30 @@
 import java.util.*;
 
 public class vegetables {
-    
-    
+
+
     public static void main(String[] args) {
-        
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Have you eaten your vegetables? (Y/N)");
+        System.out.println("Do you PINKY PROMISE to be honest?(Y/N)");
         String input = keyboard.nextLine();
-        
-        if(input.equals("Y")){
-            System.out.println("Eat more");
-        }
-        else if(input.equals("N")) {
-            System.out.println("Deleting Files....");
-        }
-        else {
-            System.out.println("Invalid Input");
+
+        if (input.equals("Y")){
+          System.out.println("thank you, 50 needles if you lied\n \n");
+          System.out.println("Have you eaten your vegetables? (Y/N)");
+          String input2 = keyboard.nextLine();
+          if(input2.equals("Y")){
+              System.out.println("Eat more");
+          }
+          else if(input2.equals("N")) {
+              System.out.println("Deleting Files....");
+          }
+          else {
+              System.out.println("Invalid Input");
+          }
+        } else if (input.equals("N")) {
+          System.out.println("go home");
+        } else {
+          System.out.println("Answer bitch");
         }
     }
 }
